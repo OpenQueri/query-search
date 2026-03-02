b5d1ed1812b11f5cc400eb29169e467ea1de65fc
Kiril <csgocvadimkiril80@gmail.com>
2026-03-02 02:45:39 +0100

    edit main

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use cld3::cld3::cld3_main;
 use extract_words::extract_words::extract_words;
 use stremmer::stremer_main::stremer_main;
 use engine::engine_main::{EngineEdit, EngineSearch};
+use std::time::Instant;
 
 #[derive(Debug)]
 pub struct Request<'a>{
@@ -21,11 +22,36 @@ pub struct Request<'a>{
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
 
-    let link = "https://www.google.com/";
-    let linke = "https://www.googl4.com/";
-    let linker = "https://www.goo4gl4.com/";
 
-    let text = "Чи не здається тобі, що коли я сьогодні пізно ввечері після довгого та виснажливого робочого дня нарешті сяду в той старий, трохи іржавий, але такий рідний автомобіль та поїду через все місто додому, де на мене чекає тепла постіль, свіжозварена кава та улюблений кіт, який вже напевно засумував, то це буде найкращим завершенням цього нескінченного дня?";
+
+    let link= vec!["https://www.google.com/","https://www.googl4.com/","https://www.goo4gl4.com/"];
+
+    let text = vec!["Знаєш, іноді після чергового нескінченного робочого марафону, коли вже сил майже не лишилось, я сідаю в свою стару машинку, яка скрипить і пахне бензином, і просто їду додому через вечірнє місто. Там мене чекає м’яке ліжко, гарячий чай з м’ятою, і мій пухнастий кіт, який весь день сумно нявкав біля дверей. І в цю мить здається, що все на світі знову стало на свої місця.",
+    "Після цілого дня за комп’ютером, коли очі вже болять, а спина ниє, я заводжу свій пошарпаний, але надійний автомобіль і повільно їду через затори додому. Вдома вже готова тепла ковдра, щойно заварена кава з корицею і мій улюблений сірий котяра, який крутиться під ногами і вимагає уваги. Саме ці прості речі роблять вечір по-справжньому хорошим.",
+    "Буває, що після виснажливого дня в офісі я ледве дочікуюсь моменту, коли сяду за кермо своєї старенької машини й поїду додому. Місто вже світиться вогнями, а я думаю тільки про те, як зараз ляжу в затишне ліжко, вип’ю ароматної кави і погладжу свого пухнастика, який весь день чекав мене біля вікна. Ось заради таких вечорів і живеш."];
+    
+    for (link, text) in link.iter().zip(text.iter()) {
+
+        let (request, link) = match  request(text, link).await {
+            Ok(val) => val,
+            Err(e) => return Err(e), 
+        };
+        EngineEdit::engine_write(&link, &request).await?;
+    }
+
+    let extra: Vec<(String, usize)> = extract_words(&[&text[0]]).await?;
+
+    let start = Instant::now();
+    EngineSearch::engine_search(extra).await?;
+    let duration = start.elapsed();
+
+    println!("Выполнилось за: {:?}", duration);
+    Ok(())
+}
+
+
+async fn request<'a, 'b>(text: &'a str, link:&'b str ) -> Result<(Request<'b>, &'b str), Box<dyn Error>>{
+
 
     let result_cld3_main = cld3_main(&text).await?;
 
@@ -34,17 +60,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let stremer_main = stremer_main(&result_cld3_main, &result_extract_words).await?;
 
     let request = Request{
-        launge: result_cld3_main,
+        launge: &result_cld3_main,
         words: stremer_main,
     };
 
-    EngineEdit::engine_write(&link, &request).await?;
-    EngineEdit::engine_write(&linke, &request).await?;
-    EngineEdit::engine_write(&linker, &request).await?;
 
-    EngineSearch::engine_search(&request).await?;
-
-
-
-    Ok(())
+    Ok((request,&link))
 }
